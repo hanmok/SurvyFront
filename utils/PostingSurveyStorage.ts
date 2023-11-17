be7de1a69c5a0c3041082ca50d1d7e33a075c153
hanmok <>
2023-11-17 23:24:33 +0900

    encapsulate PostingSurveyStorage, clean up

--- utils/PostingSurveyStorage.ts
+++ utils/PostingSurveyStorage.ts
@@ -1,113 +1,44 @@
-import { logObject } from "./Log";
-// PostingSurvey
 import Storage from "react-native-storage";
-import { storage } from "./Storage";
 import { PostingSurveyState } from "../interfaces/PostingSurveyState";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// export const initializePostingSurvey = async () => {
-//     await storage.save({
-//         key: "postingSurveys",
-//         data: [],
-//         expires: null,
-//     });
-// };
+class PostingSurveyDataManager {
+	private storage: Storage;
 
-// export const deletePostingSurvey = async (postingSurveyId: number) => {
-//     console.log(
-//         `delePostingSurvey called, postingSurveyId: ${postingSurveyId}`
-//     );
-//     const prevData = await loadSavedPostingSurveys();
+	constructor() {
+		this.storage = new Storage({
+			storageBackend: AsyncStorage,
+		});
+	}
 
-//     const updatedData = prevData.filter(
-//         postingSurvey => postingSurvey.id !== postingSurveyId
-//     );
+	initialize = async () => {
+		await this.storage.save({
+			key: "postingSurvey",
+			data: null,
+			expires: null,
+		});
+	};
 
-//     if (updatedData.length < prevData.length) {
-//         console.log("postingSurvey deleted!");
-//         await storage.save({
-//             key: "postingSurveys",
-//             data: updatedData,
-//             expires: null,
-//         });
-//     } else {
-//         console.log("postingSurvey not found");
-//     }
-// };
+	save = async (data: PostingSurveyState) => {
+		await this.storage.save({
+			key: "postingSurvey",
+			data: data,
+			expires: null,
+		});
+	};
 
-// // 지금까지 .. 저장했던 것들과 함께 다같이 저장
-// export const savePostingSurvey = async (data: PostingSurveyState) => {
-//     logObject("savePostingSurvey called, data: ", data);
+	load = async (): Promise<PostingSurveyState | null> => {
+		try {
+			const postingSurvey: PostingSurveyState = await this.storage.load({
+				key: "postingSurvey",
+				autoSync: true,
+				syncInBackground: true,
+			});
+			return postingSurvey;
+		} catch (error) {
+			return null;
+		}
+	};
+}
 
-//     const prevData = await loadSavedPostingSurveys();
-
-//     let duplicate = prevData.findIndex(prev => prev.id === data.id);
-
-//     if (duplicate === -1) {
-//         // 새로 저장
-//         await storage.save({
-//             key: "postingSurveys",
-//             data: [...prevData, data],
-//             // expires
-//             expires: null,
-//         });
-//     } else {
-//         //  수정 후 저장, updated: 중복 제거 후 data
-//         const updated = prevData.splice(duplicate, 1);
-//         await storage.save({
-//             key: "postingSurveys",
-//             data: [...updated, data],
-//             expires: null,
-//         });
-//     }
-// };
-
-// export const loadSavedPostingSurveys = async (): Promise<
-//     PostingSurveyState[]
-// > => {
-//     try {
-//         const postingSurveys: PostingSurveyState[] = await storage.load({
-//             key: "postingSurveys",
-//             autoSync: true,
-//             syncInBackground: true,
-//         });
-
-//         return postingSurveys;
-//     } catch (error) {
-//         console.error(error);
-//         return [];
-//     }
-// };
-
-export const initializePostingSurvey = async () => {
-    await storage.save({
-        key: "postingSurvey",
-        data: null,
-        expires: null,
-    });
-};
-
-export const savePostingSurvey = async (data: PostingSurveyState) => {
-    logObject("savePostingSurvey called, data: ", data);
-
-    await storage.save({
-        key: "postingSurvey",
-        data: data,
-        expires: null,
-    });
-};
-
-export const loadPostingSurvey =
-    async (): Promise<PostingSurveyState | null> => {
-        try {
-            const postingSurvey: PostingSurveyState = await storage.load({
-                key: "postingSurvey",
-                autoSync: true,
-                syncInBackground: true,
-            });
-
-            return postingSurvey;
-        } catch (error) {
-            // console.error(error);
-            return null;
-        }
-    };
+export const postingSurveyDataManager = new PostingSurveyDataManager();

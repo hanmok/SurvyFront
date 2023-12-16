7ca9616d0f42e03f6b70eba7d18e2aa7aa5831a4
hanmok <dmammmm@naver.com>
2023-12-16 15:10:47 +0900

    refactor withdrawalService

---
+++ API/Services/WithdrawalService.ts
@@ -0,0 +1,28 @@
+import { Withdrawal } from "../../interfaces/Withdrawal";
+import { API_BASE_URL } from "../API";
+import BaseApi from "../BaseAPI";
+
+export class WithdrawalService extends BaseApi {
+    async createWithdrawal(
+        accessToken: string,
+        user_id: number,
+        amount: number
+    ): Promise<ApiResponse<Withdrawal>> {
+        const url = `${API_BASE_URL}/withdrawal`;
+        const body = { user_id, amount };
+        return this.fetchData<Withdrawal>(url, "POST", body, accessToken);
+    }
+
+    async fetch(
+        accessToken: string,
+        userId: number
+    ): Promise<ApiResponse<Withdrawal[]>> {
+        const url = `${API_BASE_URL}/withdrawal/${userId}`;
+        return this.fetchData<Withdrawal[]>(url, "GET", undefined, accessToken);
+    }
+
+    async patch(accessToken: string, id: number) {
+        const url = `${API_BASE_URL}/withdrawal/${id}`;
+        return this.fetchData(url, "PATCH", undefined, accessToken);
+    }
+}
--- API/WithdrawalAPI.ts
+++ API/WithdrawalAPI.ts
@@ -1,41 +1,41 @@
-import { Withdrawal } from "../interfaces/Withdrawal";
-import { API_BASE_URL } from "./API";
-import { fetchData } from "./BaseAPI";
+// import { Withdrawal } from "../interfaces/Withdrawal";
+// import { API_BASE_URL } from "./API";
+// import { fetchData } from "./BaseAPI";
 
-export const createWithdrawal = async (
-    accessToken: string,
-    user_id: number,
-    amount: number
-) => {
-    const url = `${API_BASE_URL}/withdrawal`;
-    const data = { user_id, amount };
-    return fetchData<Withdrawal>(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify(data),
-    });
-};
+// // export const createWithdrawal = async (
+// //     accessToken: string,
+// //     user_id: number,
+// //     amount: number
+// // ) => {
+// //     const url = `${API_BASE_URL}/withdrawal`;
+// //     const data = { user_id, amount };
+// //     return fetchData<Withdrawal>(url, {
+// //         method: "POST",
+// //         headers: {
+// //             "Content-Type": "application/json",
+// //             Authorization: `Bearer ${accessToken}`,
+// //         },
+// //         body: JSON.stringify(data),
+// //     });
+// // };
 
-export const fetchWithdrawals = async (accessToken: string, userId: number) => {
-    const url = `${API_BASE_URL}/withdrawal/${userId}`;
-    return fetchData<Withdrawal[]>(url, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-};
+// export const fetchWithdrawals = async (accessToken: string, userId: number) => {
+//     const url = `${API_BASE_URL}/withdrawal/${userId}`;
+//     return fetchData<Withdrawal[]>(url, {
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${accessToken}`,
+//         },
+//     });
+// };
 
-export const patchWithdrawal = async (accessToken: string, id: number) => {
-    const url = `${API_BASE_URL}/withdrawal/${id}`;
-    return fetchData(url, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-};
+// export const patchWithdrawal = async (accessToken: string, id: number) => {
+//     const url = `${API_BASE_URL}/withdrawal/${id}`;
+//     return fetchData(url, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${accessToken}`,
+//         },
+//     });
+// };

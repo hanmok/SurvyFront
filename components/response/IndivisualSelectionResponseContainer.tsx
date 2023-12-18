f7c3ee2e0b381ecf9f7f5fb465d573fd3e995ac0
hanmok <dmammmm@naver.com>
2023-12-18 16:34:53 +0900

    fix indivisual response screen to have answerText

--- components/response/IndivisualSelectionResponseContainer.tsx
+++ components/response/IndivisualSelectionResponseContainer.tsx
@@ -3,7 +3,8 @@ import { QuestionResponseContainerProps } from "./OverallSelectionResponseContai
 import { getQuestionType } from "../../QuestionType";
 import { fontSizes } from "../../utils/sizes";
 import { useEffect, useState } from "react";
-import { GQLSelectableOption } from "../../interfaces/GQLInterface";
+import { GQLAnswer, GQLSelectableOption } from "../../interfaces/GQLInterface";
+import { logObject } from "../../utils/Log";
 
 const IndivisualSelectionResponseContainer: React.FC<
     QuestionResponseContainerProps
@@ -18,16 +19,36 @@ const IndivisualSelectionResponseContainer: React.FC<
         GQLSelectableOption[]
     >([]);
 
+    const [selectedAnswers, setSelectedAnswers] = useState<GQLAnswer[]>([]);
+
     useEffect(() => {
         const anss = answers.filter(
             answer => answer.user.id === selectedUserId
         );
 
+        logObject("\nall Answers flag 1", answers);
+        logObject("anss", anss);
+        // [{"id":"3074",
+        // "question":{"id":"457214"},
+        // "selectableOption":{"id":"469894","value":"기타"},
+        // "user":{"id":"804"},
+        // "answerText":"Ccc"}]
+
         const allSelectableOptionIds = anss.map(ans => ans.selectableOption.id);
+        logObject("selectableOptions", selectableOptions);
+        // [{"id":"469884","position":0,"value":"S1q1so1","isExtra":0},
+        // {"id":"469894","position":1,"value":"기타","isExtra":1}]
 
         const selectedOptionsOnly = selectableOptions.filter(so =>
             allSelectableOptionIds.includes(so.id)
         );
+
+        setSelectedAnswers(anss);
+
+        logObject("gqlSelectableOptions", selectedOptionsOnly);
+        console.log("");
+
+        // [{"id":"469894","position":1,"value":"기타","isExtra":1}]
         setSelectedSelectableOptions(selectedOptionsOnly);
     }, [selectedUserId]);
 
@@ -42,9 +63,16 @@ const IndivisualSelectionResponseContainer: React.FC<
                 <Text style={{ fontSize: fontSizes.l24 }}>{questionTitle}</Text>
                 <View style={{ height: 10 }} />
                 <FlatList
-                    data={selectedSelectableOptions}
+                    // data={selectedSelectableOptions}
+                    data={selectedAnswers}
                     renderItem={({ item }) => (
-                        <Text style={{ marginLeft: 10 }}>{item.value}</Text>
+                        // <Text style={{ marginLeft: 10 }}>{item.value}</Text>
+                        <Text>
+                            {item.answerText !== ""
+                                ? item.answerText
+                                : item.selectableOption.value}
+                            {/* {item.answerText} {item.selectableOption.value} */}
+                        </Text>
                     )}
                     keyExtractor={item => `${item.id}`}
                     ListFooterComponent={<View style={{ height: 10 }} />}

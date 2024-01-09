fd70d035225b20a0d388f4956e77b3962e470213
hanmok <dmammmm@naver.com>
2024-01-09 23:09:02 +0900

    make ref for each TextInput, has index difference by 1..

--- components/posting/DynamicTextInputsForCreation.tsx
+++ components/posting/DynamicTextInputsForCreation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
     View,
     TextInput,
@@ -18,6 +18,12 @@ const DynamicTextInputsForCreation = ({
     isExtraOptionEnabled,
 }) => {
     const [inputValues, setInputValues] = useState([""]);
+    // const inputRefs = useRef([React.createRef()]);
+    // const inputRefs = useRef([React.createRef()]);
+    const inputRefs = useRef([]);
+    // const inputRefs = useRef<Array<React.RefObject<TextInput>>>([
+    //     React.createRef(),
+    // ]);
 
     useEffect(() => {
         setDynamicInputValues(inputValues);
@@ -27,9 +33,58 @@ const DynamicTextInputsForCreation = ({
         setInputValues(dynamicInputValues);
     }, []);
 
+    // useEffect(() => {
+    //     const newRef = React.createRef();
+
+    //     // inputRefs.current.push(React.createRef());
+
+    //     // inputRefs.current[inputValues.length];
+    //     inputRefs.current.push(newRef);
+    // }, []);
+
     const handleAddInput = () => {
         setInputValues([...inputValues, ""]); // 새로운 빈 문자열을 배열에 추가
+        // const newRef = React.createRef();
+        // inputRefs.current.push(newRef);
+        inputRefs.current.push(React.createRef());
     };
+    // 랜더링 되기 전에 focus 가 호출되므로 동작하지 않는다.
+    // 따라서, TextInput 이 마운트 된 후에 focus 를 호출해야한다.
+    // 그렇게 하기 위해서는 'useEffect' 훅을 사용하여 비동기적으로 처리해야한다.
+
+    // useEffect(() => {
+    //     if (inputRefs.current.length < inputValues.length) {
+    //         const newRef = React.createRef();
+    //         inputRefs.current.push(newRef);
+    //     }
+    //     console.log(
+    //         `num of inputs: ${inputValues.length}, num of refs: ${inputRefs.current.length}`
+    //     );
+    //     if (
+    //         inputRefs.current.length > 0 &&
+    //         inputRefs.current[inputRefs.current.length - 1].current
+    //     ) {
+    //         inputRefs.current[inputRefs.current.length - 1].current.focus();
+    //         console.log(`focused index: ${inputRefs.current.length - 1}`);
+    //     }
+    // }, [inputRefs.current.length]);
+
+    // useEffect(() => {
+    //     const newRef = React.createRef();
+    //     inputRefs.current.push(newRef);
+    // }, []);
+
+    useEffect(() => {
+        // if (inputValues.length !== inputRefs.current.length) {
+        //     const newRef = React.createRef();
+        //     inputRefs.current.push(newRef);
+        // }
+
+        const lastInputRef = inputRefs.current[inputRefs.current.length - 1];
+        if (lastInputRef && lastInputRef.current) {
+            lastInputRef.current.focus();
+        }
+    }, [inputValues.length]);
 
     const handleInputChange = (text, index) => {
         const newInputValues = [...inputValues];
@@ -42,6 +97,7 @@ const DynamicTextInputsForCreation = ({
         const newInputValues = [...inputValues];
         newInputValues.splice(index, 1); // 해당 인덱스의 입력 값을 제거
         setInputValues(newInputValues);
+        inputRefs.current.splice(index, 1); // 해당 인덱스의 ref 도 제거.
     };
 
     const dismissKeyboard = () => {
@@ -55,6 +111,7 @@ const DynamicTextInputsForCreation = ({
                     <View key={index} style={styles.inputContainer}>
                         <TextInput
                             key={keys[index]}
+                            ref={inputRefs.current[index]} // ref 연결
                             placeholder={`옵션 ${index + 1}`}
                             style={styles.input}
                             value={value}
@@ -63,11 +120,14 @@ const DynamicTextInputsForCreation = ({
                             }
                             autoComplete="off"
                             autoCorrect={false}
+                            blurOnSubmit={false}
                             onSubmitEditing={() => {
                                 console.log(`index: ${index}`); // 첫번째꺼 0
-                                if (index + 1 === inputValues.length) {
-                                    handleAddInput();
-                                }
+
+                                // if (index + 1 === inputValues.length) {
+                                handleAddInput();
+
+                                // }
                             }}
                         />
                         <ImageButton
@@ -91,7 +151,9 @@ const DynamicTextInputsForCreation = ({
                     title="Add Input"
                     backgroundStyle={styles.addInputBG}
                     textStyle={styles.addInputText}
-                    onPress={handleAddInput}
+                    onPress={() => {
+                        handleAddInput();
+                    }}
                 />
             </View>
         </TouchableWithoutFeedback>

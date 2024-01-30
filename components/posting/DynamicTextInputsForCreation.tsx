19876e9f52ad94e8e92621e7526915e9c0a8babd
hanmok <dmammmm@naver.com>
2024-01-31 03:38:46 +0900

    fix posting process (Creating, Modifying Selectable Options)

--- components/posting/DynamicTextInputsForCreation.tsx
+++ components/posting/DynamicTextInputsForCreation.tsx
@@ -17,13 +17,8 @@ const DynamicTextInputsForCreation = ({
     keys,
     isExtraOptionEnabled,
 }) => {
-    const [inputValues, setInputValues] = useState([""]);
-    // const inputRefs = useRef([React.createRef()]);
-    // const inputRefs = useRef([React.createRef()]);
+    const [inputValues, setInputValues] = useState<string[]>([]);
     const inputRefs = useRef([]);
-    // const inputRefs = useRef<Array<React.RefObject<TextInput>>>([
-    //     React.createRef(),
-    // ]);
 
     useEffect(() => {
         setDynamicInputValues(inputValues);
@@ -33,53 +28,21 @@ const DynamicTextInputsForCreation = ({
         setInputValues(dynamicInputValues);
     }, []);
 
-    // useEffect(() => {
-    //     const newRef = React.createRef();
-
-    //     // inputRefs.current.push(React.createRef());
-
-    //     // inputRefs.current[inputValues.length];
-    //     inputRefs.current.push(newRef);
-    // }, []);
-
     const handleAddInput = () => {
         setInputValues([...inputValues, ""]); // 새로운 빈 문자열을 배열에 추가
-        // const newRef = React.createRef();
-        // inputRefs.current.push(newRef);
         inputRefs.current.push(React.createRef());
     };
+
+    useEffect(() => {
+        setInputValues([...inputValues, ""]); // 새로운 빈 문자열을 배열에 추가
+        inputRefs.current.push(React.createRef());
+    }, []);
+
     // 랜더링 되기 전에 focus 가 호출되므로 동작하지 않는다.
     // 따라서, TextInput 이 마운트 된 후에 focus 를 호출해야한다.
     // 그렇게 하기 위해서는 'useEffect' 훅을 사용하여 비동기적으로 처리해야한다.
 
-    // useEffect(() => {
-    //     if (inputRefs.current.length < inputValues.length) {
-    //         const newRef = React.createRef();
-    //         inputRefs.current.push(newRef);
-    //     }
-    //     console.log(
-    //         `num of inputs: ${inputValues.length}, num of refs: ${inputRefs.current.length}`
-    //     );
-    //     if (
-    //         inputRefs.current.length > 0 &&
-    //         inputRefs.current[inputRefs.current.length - 1].current
-    //     ) {
-    //         inputRefs.current[inputRefs.current.length - 1].current.focus();
-    //         console.log(`focused index: ${inputRefs.current.length - 1}`);
-    //     }
-    // }, [inputRefs.current.length]);
-
-    // useEffect(() => {
-    //     const newRef = React.createRef();
-    //     inputRefs.current.push(newRef);
-    // }, []);
-
     useEffect(() => {
-        // if (inputValues.length !== inputRefs.current.length) {
-        //     const newRef = React.createRef();
-        //     inputRefs.current.push(newRef);
-        // }
-
         const lastInputRef = inputRefs.current[inputRefs.current.length - 1];
         if (lastInputRef && lastInputRef.current) {
             lastInputRef.current.focus();
@@ -152,9 +115,7 @@ const DynamicTextInputsForCreation = ({
                     title="Add Input"
                     backgroundStyle={styles.addInputBG}
                     textStyle={styles.addInputText}
-                    onPress={() => {
-                        handleAddInput();
-                    }}
+                    onPress={handleAddInput}
                 />
             </View>
         </TouchableWithoutFeedback>
@@ -165,6 +126,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
+        // backgroundColor: "magenta",
+        backgroundColor: colors.background,
     },
     inputContainer: {
         flexDirection: "row",

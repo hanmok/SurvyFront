b2db8788e5c8ac8ed84e5d2386cccf164ffa3a40
hanmok <dmammmm@naver.com>
2024-02-06 01:18:12 +0900

    some changes for publishing process

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// import getDefaultConfig from "@react-native/metro-config";
+
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);

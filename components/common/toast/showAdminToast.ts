a3a9cba03caec1634d23bdae056bcef7200f2fa8
hanmok <dmammmm@naver.com>
2024-03-24 16:37:43 +0900

    hide adminToast

--- components/common/toast/showAdminToast.ts
+++ components/common/toast/showAdminToast.ts
@@ -2,15 +2,15 @@ import Toast, { ToastType } from "react-native-toast-message";
 import { MessageType } from "./MessageType";
 
 const showAdminToast = (
-    messageType: MessageType,
-    message: string,
-    message2?: string
+	messageType: MessageType,
+	message: string,
+	message2?: string
 ) => {
-    Toast.show({
-        type: messageType,
-        text1: message,
-        text2: message2,
-    });
+	// Toast.show({
+	//     type: messageType,
+	//     text1: message,
+	//     text2: message2,
+	// });
 };
 
 export default showAdminToast;
